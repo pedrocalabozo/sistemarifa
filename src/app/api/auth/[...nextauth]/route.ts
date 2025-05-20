b112e7a2a2a5f335d33bfe41dd2de6e984709cc6
Pedro miguel <pedrooropeza18@gmail.com>
2025-05-20 13:35:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/app/api/auth/[...nextauth]/route.ts:2:1
    > Export GET doesn't exist in target module
    >   1 |
    > > 2 | export { GET, POST } from "@/auth"
    >     | ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    >   3 | // export const runtime = "edge" // Opcional: si quieres usar Edge Functions para autenticación
    >   4 |
    >
    > The export GET was not found in module [project]/src/auth.ts [app-route] (ecmascript).
    > Did you mean to import signOut?
    > All exports of the module are statically known (It doesn't have dynamic exports). So it's known statically that the requested export doesn't exist.
    >
    >

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
 
-export { GET, POST } from "@/auth"
+import { handlers } from "@/auth";
+export const { GET, POST } = handlers;
 // export const runtime = "edge" // Opcional: si quieres usar Edge Functions para autenticación
