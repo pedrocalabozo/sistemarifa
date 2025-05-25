a43079aaddb4fc0d7c5f63d78a07c5ba7618b9db
pedrocalabozo <89891847+pedrocalabozo@users.noreply.github.com>
2025-05-25 03:03:22 -0400

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  context: { params: Promise<{ id: string }> }
+  context: { params: Record<string, string> }
 ) {
-  const { id } = await context.params; // Resolviendo la promesa correctamente
+  const { id } = context.params; // Accediendo correctamente a los parámetros
 
   if (!id) {
     return NextResponse.json({ error: "ID no proporcionado" }, { status: 400 });
